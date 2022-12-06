17cc4f2556e426e93c17af5d4538a7e21c1d5cca
Likith Shet <96128274+shet27@users.noreply.github.com>
2022-12-06 23:02:00 +0530

    Create program to find lcm using loop.py

---
+++ program to find lcm using loop.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+lcm = num1 if(num1 > num2) else num2
+
+while(True):
+    if((lcm % num1 == 0) and (lcm % num2 == 0)):
+        print("The L.C.M. of", num1,"and", num2,"is", lcm)
+        break
+    lcm += 1
