72553c503d07df01716c9cb370dac64061df9017
Likith Shet <96128274+shet27@users.noreply.github.com>
2023-02-08 16:18:24 +0530

    Update program to find lcm using loop.py

--- program to find lcm using loop.py
+++ program to find lcm using loop.py
@@ -6,6 +6,6 @@ lcm = num1 if(num1 > num2) else num2
 
 while(True):
     if((lcm % num1 == 0) and (lcm % num2 == 0)):
-        print("The L.C.M. of", num1,"and", num2,"is", lcm)  #LCM least common multiple
+        print("The L.C.M. of", num1,"and", num2,"is", lcm)  #LCM- least common multiple
         break
     lcm += 1
